ceb896e2cd180a1cef0ec548e5861906746c3f48
Jessie Hernandez <github@jessiehernandez.com>
2021-02-27 21:26:19 +0100

    Updated the parsing of the uptime command
    Sometimes the format of the uptime command is slightly diffrent.
    made the parsing a bit smarter

--- router_uptime.sh
+++ router_uptime.sh
@@ -4,6 +4,6 @@ dir=`dirname $0`
 
 name="router_uptime"
 columns="uptime load_1_min load_5_min load_15_min"
-points=$(uptime | sed 's/.*up \(.* days\), \(.*\):\(.*\),  load average: \(.*\), \(.*\), \(.*\)/\1 \2 \3 \4 \5 \6/g' |  awk '{print(($1 * 24 * 60 * 60) + ($3 * 60 * 60) + ($4 * 60)), $5, $6, $7}')
+points=$(uptime | sed 's/.*up \(.*\) days,  \(.*\)[: ]\(.*\),  load average: \(.*\), \(.*\), \(.*\)/\1 \2 \3 \4 \5 \6/g' |  awk '{if ($3 ~ /^[0-9]+$/) {print ($1 * 24 *  60 * 60) + ($2 * 60 * 60) + ($3 * 60), $4, $5, $6} else print ($1 * 24 * 60 * 60) + ($2 * 60), $4, $5, $6}')
 
 $dir/todb.sh "${name}" "${columns}" "${points}"
