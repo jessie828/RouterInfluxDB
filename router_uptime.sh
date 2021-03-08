aa7890597be343c28b794ec17613cc8c4456f4b7
Jessie Hernandez <github@jessiehernandez.com>
2021-03-08 23:53:00 +0100

    Made the uptime parsing better

--- router_uptime.sh
+++ router_uptime.sh
@@ -4,6 +4,8 @@ dir=`dirname $0`
 
 name="router_uptime"
 columns="uptime load_1_min load_5_min load_15_min"
-points=$(uptime | sed 's/.*up \(.*\) days,  \(.*\)[: ]\(.*\),  load average: \(.*\), \(.*\), \(.*\)/\1 \2 \3 \4 \5 \6/g' |  awk '{if ($3 ~ /^[0-9]+$/) {print ($1 * 24 *  60 * 60) + ($2 * 60 * 60) + ($3 * 60), $4, $5, $6} else print ($1 * 24 * 60 * 60) + ($2 * 60), $4, $5, $6}')
+time=$(cat /proc/uptime | awk '{print $1}' | sed 's/\..*//g')
+load=$(uptime | sed 's/.*,  load average: \(.*\), \(.*\), \(.*\)/\1 \2 \3/g' |  awk '{print $1, $2, $3}')
+points=$(echo $time $load)
 
 $dir/todb.sh "${name}" "${columns}" "${points}"
