0173a4400957ddd43811123df953d2b98cb0cdb9
Jessie Hernandez <github@jessiehernandez.com>
2021-03-08 23:53:40 +0100

    also add connected wired clients.
    In my case I filter out the WAN IP in order to get a better count

--- router_assoclist.sh
+++ router_assoclist.sh
@@ -15,3 +15,21 @@ columns="count"
 p1=`wl -i eth6 assoclist | awk '{print $2}' | wc -l`
 points="$p1"
 $dir/todb.sh "$name" "$columns" "$points"
+
+name="router_assoclist_wired"
+columns="count"
+two=$(wl -i eth5 assoclist | awk '{print $2}' | tr '[A-Z]' '[a-z]')
+five=$(wl -i eth6 assoclist | awk '{print $2}' | tr '[A-Z]' '[a-z]')
+all=$(arp -n | grep -v 192.168.178.1 |  awk '{print $4}')
+for mac in ${all}
+do
+    echo ${two} ${five} | grep ${mac} >/dev/null
+    if [ $? -eq 1 ]
+    then
+        echo ${mac} >> /tmp/router_assoclist_wired.txt
+    fi
+done
+p1=$(cat /tmp/router_assoclist_wired.txt | wc -l)
+points="$p1"
+$dir/todb.sh "$name" "$columns" "$points"
+rm /tmp/router_assoclist_wired.txt
